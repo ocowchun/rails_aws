9aad63bd91c2e71c5ba8ca451dacb9611a8b452c
ocowchun <ocowchun@gmail.com>
2014-03-10 10:28:26 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAws::Application.config.secret_token = '100bb76476ed824081982bd88f6466979990749a65f3b8dee6032b081c6b7bfcaafb46713a0893df01c7c49258657b074686c97484c76c5c43a9cbe1103683dc'
